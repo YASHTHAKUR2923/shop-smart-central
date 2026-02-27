696f4cbe34cbb17fa4d1d995122f9b2a0c70820f
Anant Vashishta <anant@zillionnmicrofinance.com>
2026-02-27 16:41:55 +0530

    changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -90,7 +90,7 @@ export default function Index() {
 </div>
 {/* Moving CTA Banner */}
 {/* CTA Section */}
-<section className="py-16 lg:py-24 bg-transparent overflow-hidden">
+<section className="py-16 lg:py-15 bg-transparent overflow-hidden">
   <div className="container text-center">
 
     {/* Moving Ticker Line */}
